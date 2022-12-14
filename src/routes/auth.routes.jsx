a9778f756235e5c8a4a6cc1158bb0a8a97a77a32
Lightz-11 <lucasanes007@outlook.com>
2022-12-14 14:54:49 -0300

    fix: portraitLink

--- src/routes/auth.routes.jsx
+++ src/routes/auth.routes.jsx
@@ -1,10 +1,12 @@
 import { Routes, Route } from "react-router-dom";
 import { Home } from "../pages/Home";
+import { Portrait } from "../pages/Portrait";
 
 export function AuthRoutes() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="/ficha/portrait/:id" element={<Portrait />} />
     </Routes>
   );
 }
