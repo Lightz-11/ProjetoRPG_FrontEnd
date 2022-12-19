47af410b2f5ca8708ace278e8c71b7cf2d9df44b
Lightz-11 <lucasanes007@outlook.com>
2022-12-19 12:47:43 -0300

    fix: api

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "projetorpgbackend-production-986c.up.railway.app"
+  baseURL: "https://projetorpgbackend-production-986c.up.railway.app"
   //baseURL: "http://localhost:8080"
 });
