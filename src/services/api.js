d18d23740e9cd34f21185c2d1218463868e55c55
Lightz-11 <lucasanes007@outlook.com>
2023-01-11 13:10:10 -0300

    api

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const api = axios.create({
-  //baseURL: "https://projetorpgbackend-production-986c.up.railway.app"
-  baseURL: "http://localhost:8080"
+  baseURL: "https://projetorpgbackend-production-986c.up.railway.app"
+  //baseURL: "http://localhost:8080"
 });
